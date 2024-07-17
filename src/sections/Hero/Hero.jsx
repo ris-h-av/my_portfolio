98fa3413e83b76c3d9f5751e1d4fda87a37b4d4b
Risab Kumar <risabk444@gmail.com>
2024-07-17 20:54:08 +0530

    video timestamp = 31:57

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,6 +1,9 @@
 import styles from "./heroStyles.module.css";
-import heroImg from "../../assets/hero-img.png";
+import heroImg from "../../assets/hero-img.jpg";
 import themeIcon from "../../assets/sun.svg"
+import linkedinIcon from "../../assets/linkedin-light.svg"
+import twitterIcon from "../../assets/twitter-light.svg"
+import githubIcon from "../../assets/github-light.svg"
 
 function Hero() {
   return (
@@ -11,6 +14,24 @@ function Hero() {
       </div>
       <div className={styles.info}>
         <h1>Risab Kumar</h1>
+        <h2>Backend Developer</h2>
+        <span>
+          <a href="https://in.linkedin.com/" target="_blank" alt="linked in">
+            <img src={linkedinIcon} alt="linkedin.com" />
+          </a>
+          <a href="https://x.com/" target="_blank" alt="twitter">
+            <img src={twitterIcon} alt="twiiter.com" />
+          </a>
+          <a href="https://github.com/" target="_blank" alt="github">
+            <img src={githubIcon} alt="github.com" />
+          </a>
+        </span>
+        <p>
+          With a passion to change the world. 
+        </p>
+        {/* <a href={CV}>
+          <button className="hover"></button>
+        </a> */}
       </div>
     </section>
   );
